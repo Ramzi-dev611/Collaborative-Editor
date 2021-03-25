76236cd739304438947553f8725be1631944d1e6
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2021-03-25 11:56:38 +0100

    Start from Scratch

--- src/main/java/MainSendings.java
+++
@@ -1,6 +0,0 @@
-public class MainSendings {
-    public static void main(String[] args) {
-        new Sender("editeur1", "User One Prompt");
-        new Sender ("editeur2", "User Two Prompt");
-    }
-}

1bbbae430c253e0df7d4b887223ee0cbff4b2c2f
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2021-04-04 23:25:56 +0100

    final version of welcoming page

--- src/main/java/WelcomeFrame.java
+++ src/main/java/WelcomeFrame.java
@@ -4,68 +4,108 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class WelcomeFrame extends JFrame implements FocusListener {
-    protected JTextField input;
-    protected JButton connect;
-    protected JLabel err;
-    public WelcomeFrame (){
-        //err
-        err = new JLabel("Please type a valid name");
-        err.setForeground(Color.red);
-        err.setBounds(110, 115, 200, 50);
-        err.setVisible(false);
-        // input Field
-        input = new JTextField("Type you name here");
-        input.setForeground(Color.gray);
-        input.setFont(new Font("Boli MV", Font.ITALIC, 22));
-        input.addFocusListener(this);
-        input.setBounds(110,175,200,70);
-        // Submit  button
-        connect =new JButton("Sign in");
-        connect.setFont(new Font("Helvatica",Font.BOLD, 18));
-        connect.setBackground(new Color(0x3bb357));
-        connect.setForeground(Color.white);
-        connect.setFocusable(false);
-        connect.addActionListener(e -> {
-            String name = input.getText();
-            if (name.equals("")){
-                input.setBorder(BorderFactory.createLineBorder(Color.red, 3));
-                err.setVisible(true);
-            }else {
-                this.dispose();
-                new Sender(name, "");
+    protected JPanel panLeft;
+    protected JTextArea leftLabel;
+    protected ImageIcon logo;
+
+    protected JPanel panRight ;
+    protected JLabel errorMessage;
+    protected JTextField queueName;
+    protected JButton signIn;
+
+    private void buildZoneLeft(){
+        panLeft = new JPanel();
+        panLeft.setLayout(null);
+        leftLabel = new JTextArea();
+        leftLabel.setText("Welcome To Collab-editor, the BEST place for you to CO-WORK \n"
+        +"Insert your prefered username and join your Coworkersto create and inovate \n"+
+                "Good Luck !");
+        leftLabel.setEditable(false);
+        leftLabel.setBorder(BorderFactory.createEmptyBorder());
+        leftLabel.setBackground(new Color(0x021647));
+        leftLabel.setFont(new Font("Helvatica", Font.PLAIN, 20));
+        leftLabel.setForeground(Color.white);
+        leftLabel.setLineWrap(true);
+        leftLabel.setBounds(100, 400, 486, 500);
+        logo = new ImageIcon("assets/logo.png");
+        JLabel l = new JLabel();
+        l.setBounds(280, 160, 320, 320);
+        l.setIcon(logo);
+        panLeft.add(l);
+        panLeft.add(leftLabel);
+        panLeft.setBackground(new Color(0x021647));
+    }
+    private void buildZoneRight(){
+        panRight = new JPanel();
+        panRight.setLayout(null);
+        errorMessage = new JLabel("Please insert a username before loging in !");
+        errorMessage.setForeground(Color.RED);
+        errorMessage.setBackground(new Color(0xffb0b0));
+        errorMessage.setBorder(BorderFactory.createLineBorder(new Color(0xffb0b0),5,true));
+        errorMessage.setFont(new Font("Helvatica", Font.PLAIN,20));
+        errorMessage.setOpaque(true);
+        errorMessage.setVisible(false);
+        errorMessage.setBounds(120,200,400,60);
+
+        queueName = new JTextField("Your UserName !! ");
+        queueName.setForeground(Color.gray);
+        queueName.setFont(new Font("Helvatica", Font.ITALIC, 20));
+        queueName.setHorizontalAlignment(SwingConstants.CENTER);
+        queueName.addFocusListener(this);
+        queueName.setBounds(120, 300, 500, 60);
+
+        signIn = new JButton("Sign UP");
+        signIn.setBackground(Color.green);
+        signIn.setForeground(Color.white);
+        signIn.setFocusable(false);
+        signIn.addActionListener(e ->{
+            if(queueName.getText().equals("") || queueName.getText().equals("Your UserName !! ")) {
+                errorMessage.setVisible(true);
+                queueName.grabFocus();
+            }
+            else{
+                new Sender(queueName.getText(), "");
             }
         });
-        connect.setBounds(160,300, 100, 50);
+        signIn.setBounds(320, 400,120,50);
 
-        setTitle("Welcome to The Collaborative Editor");
-        setLayout(null);
-        setSize(420,420);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(err);
-        add(input);
-        add(connect);
+        panRight.add(queueName);
+        panRight.add(errorMessage);
+        panRight.add(signIn);
+        panRight.setBackground(new Color(0x021647));
+    }
+
+    public WelcomeFrame (){
+        buildZoneLeft();
+        buildZoneRight();
+        setTitle("Welcome to Collab-Editor");
+        getContentPane().setBackground(new Color(0xE6021647, true));
+        setExtendedState(MAXIMIZED_BOTH);
+        setLayout(new GridLayout(1,2, 10,0));
+        add(panLeft);
+        add(panRight);
+        setResizable(false);
         setVisible(true);
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     @Override
     public void focusGained(FocusEvent e) {
-        if(e.getSource()==input){
-            if(input.getForeground()== Color.gray){
-                input.setText("");
-                input.setForeground(Color.black);
-            }
+        if(queueName.getText().equals("Your UserName !! ")){
+            queueName.setText("");
+            queueName.setForeground(Color.BLACK);
         }
     }
 
     @Override
     public void focusLost(FocusEvent e) {
-        if(e.getSource()==input){
-            if(input.getText().equals("")){
-                input.setText("Name your new room");
-                input.setForeground(Color.gray);
-            }
+        if(queueName.getText().equals("")){
+            queueName.setText("Your UserName !! ");
+            queueName.setForeground(Color.gray);
         }
+    }
 
+    public static void main (String []args){
+        new WelcomeFrame();
     }
 }
